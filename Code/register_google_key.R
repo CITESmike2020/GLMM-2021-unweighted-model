6478f107a9c22fc22680e815a3c529b0302d904e
CITESmike2020 <74291831+CITESmike2020@users.noreply.github.com>
2021-11-02 08:59:33 +0300

    Create register_google_key.R

---
+++ Code/register_google_key.R
@@ -0,0 +1,5 @@
+# use the key so that we can access GoogleMaps etc
+# for more detail go to link:
+# https://cran.r-project.org/web/packages/ggmap/readme/README.html
+
+register_google(key = "INSERT YOUR GOOGLE MAPS KEY HERE")
