62afa2d8a8953189ec825f0c079f795effdb8ef7
CITESmike2020 <74291831+CITESmike2020@users.noreply.github.com>
2021-11-02 08:58:45 +0300

    Delete register_google_key.R

--- Code/register_google_key.R
+++
@@ -1,5 +0,0 @@
-# use the key so that we can access GoogleMaps etc
-# for more detail go to link:
-# https://cran.r-project.org/web/packages/ggmap/readme/README.html
-
-register_google(key = "INSERT YOUR GOOGLE MAPS KEY HERE")
